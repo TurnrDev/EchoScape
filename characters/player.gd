06d75a8f397630cf3681b3a1986731953adb8ac3
Jay Turner <jaynicholasturner@gmail.com>
2023-07-20 16:15:49 +0100

    feat: Add character name selection and display in shop scene
    This commit adds the functionality to select a character name randomly from a list of choices (Gary, Adam, Eric, James) and display it in the shop scene. The player's character name is set in the `set_player_name()` function called during `_ready()`. The shop scene's RichTextLabel now displays a message that includes the selected character name.

--- characters/player.gd
+++ characters/player.gd
@@ -6,6 +6,17 @@ var facing: Vector2 = Vector2.DOWN
 const cardinal_directions: Array[Vector2] = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
 const texture_dir_map = {Vector2.LEFT: 2, Vector2.RIGHT: 0, Vector2.UP: 1, Vector2.DOWN: 3}
 
+var character_name: String
+
+func set_player_name():
+	var choices: Array[String] = ["Gary", "Adam", "Eric", "James"]
+	var rng = RandomNumberGenerator.new()
+	var index = rng.randi_range(0, 3)
+	character_name = choices[index]
+	
+func _ready():
+	set_player_name()
+
 func _physics_process(delta):
 	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
--- scenes/shop.gd
+++ scenes/shop.gd
@@ -3,10 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$CanvasLayer/Panel/RichTextLabel.text = (
+		"This is {name}. This is you. {name} is unimportant, so unimportant in fact, that his name was pulled at random. Don't believe me? Close the game and start again. See!"
+		. format({"name": $player.character_name})
+	)
 	$CanvasLayer/Panel/RichTextLabel.visible_characters = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $CanvasLayer/Panel/RichTextLabel.visible_characters < $CanvasLayer/Panel/RichTextLabel.text.length():
+	if (
+		$CanvasLayer/Panel/RichTextLabel.visible_characters
+		< $CanvasLayer/Panel/RichTextLabel.text.length()
+	):
 		$CanvasLayer/Panel/RichTextLabel.visible_characters += 1
